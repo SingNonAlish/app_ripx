d6880cd4e3b01332fff9bde7205438ca198ff363
Krusing <singha2166@gmail.com>
2021-05-09 12:46:21 +0700

    initail commit

--- liff-starter.js
+++ liff-starter.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
     const defaultLiffId = "1655966301-PnRDNEYB";   // change the default LIFF value if you are not using a node server
 
-    // DO NOT CHANGE THIS
+    //DO NOT CHANGE THIS
     let myLiffId = "";
 
     // if node is used, fetch the environment variable and pass it to the LIFF method
